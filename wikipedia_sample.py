a6650efaed17764149418ff04d819eea9aa6effe
fukuda <fukuda.m0113@icloud.com>
2021-05-30 19:20:14 +0900

    道順出るように

--- wikipedia_sample.py
+++ wikipedia_sample.py
@@ -11,7 +11,6 @@ def dfs(pages, links, arrived_time):
   while stack:
     index_tmp = stack.pop()
     for index_neighbor in links[index_tmp]:
-      
       if arrived_time[index_neighbor] < 0:
         arrived_time[index_neighbor] = arrived_time[index_tmp] + 1
         if index_neighbor in links:
@@ -21,7 +20,7 @@ def dfs(pages, links, arrived_time):
   
   return arrived_time[index_item]
 
-def bfs(pages, links, arrived_time):
+def bfs(pages, links, arrived_time, prev_spot):
   for k, v in pages.items():
     if v == 'Google':
       index_google = k
@@ -34,17 +33,17 @@ def bfs(pages, links, arrived_time):
   while queue:
     index_tmp = queue.pop(0)
     for index_neighbor in links[index_tmp]:
-      
       if arrived_time[index_neighbor] < 0:
         arrived_time[index_neighbor] = arrived_time[index_tmp] + 1
+        prev_spot[index_neighbor] = index_tmp
         if index_neighbor in links:
           queue.append(index_neighbor)
       else:
-        arrived_time[index_neighbor] = min(arrived_time[index_neighbor], arrived_time[index_tmp] + 1)
-  
-  return arrived_time[index_item]
+        if arrived_time[index_neighbor] > arrived_time[index_tmp] + 1:
+          arrived_time[index_neighbor] = arrived_time[index_tmp] + 1
+          prev_spot[index_neighbor] = index_tmp
 
-
+  return arrived_time, prev_spot, index_item
 
 def main():
   pages = {}
@@ -57,8 +56,6 @@ def main():
       # page[0]: id, page[1]: title
       pages[int(page[0])] = page[1]
       max_page_id = max(max_page_id, int(page[0]))
-      # if max_page_id < page[0]:
-      #   max_page_id = page[0]
 
   with open('data/links_small.txt') as f:
     for data in f.read().splitlines():
@@ -71,9 +68,17 @@ def main():
         links[link[0]] = {link[1]}
 
   arrived_time = [-1] * max_page_id
-  print(dfs(pages, links, arrived_time))
-  print(bfs(pages, links, arrived_time))
-
+  prev_spot = [""] * max_page_id
+  # print(dfs(pages, links, [-1] * max_page_id))
+  # print(bfs(pages, links, [-1] * max_page_id, [-1] * max_page_id))
+  
+  route = []
+  arrived_time, prev_spot, tmp = bfs(pages, links, arrived_time, prev_spot)
+  while pages[tmp] != "Google":
+    route.append(pages[tmp])
+    tmp = prev_spot[tmp]
+  route.append(pages[tmp])
+  print(route)
 
 
 if __name__ == '__main__':
