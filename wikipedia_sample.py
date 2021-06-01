321da166bd1d13594b08811e174a093d5865a76a
fukuda <fukuda.m0113@icloud.com>
2021-06-01 21:48:46 +0900

    error検出

--- wikipedia_sample.py
+++ wikipedia_sample.py
@@ -1,24 +1,4 @@
-def dfs(pages, links, arrived_time, searched_word):
-  for k, v in pages.items():
-    if v == 'Google':
-      index_google = k
-    if v == searched_word:
-      index_item = k
-      
-  arrived_time[index_google] = 0
-  stack = [index_google]
-  
-  while stack:
-    index_tmp = stack.pop()
-    for index_neighbor in links[index_tmp]:
-      if arrived_time[index_neighbor] < 0:
-        arrived_time[index_neighbor] = arrived_time[index_tmp] + 1
-        if index_neighbor in links:
-          stack.append(index_neighbor)
-      else:
-        arrived_time[index_neighbor] = min(arrived_time[index_neighbor], arrived_time[index_tmp] + 1)
-  
-  return arrived_time[index_item]
+import sys
 
 def bfs(pages, links, arrived_time, prev_spot, searched_word):
   for k, v in pages.items():
@@ -26,38 +6,43 @@ def bfs(pages, links, arrived_time, prev_spot, searched_word):
       index_google = k
     if v == searched_word:
       index_item = k
-      
-  arrived_time[index_google] = 0
-  queue = [index_google]
-  
-  while queue:
-    index_tmp = queue.pop(0)
-    for index_neighbor in links[index_tmp]:
-      if arrived_time[index_neighbor] < 0:
-        arrived_time[index_neighbor] = arrived_time[index_tmp] + 1
-        prev_spot[index_neighbor] = index_tmp
-        if index_neighbor in links:
-          queue.append(index_neighbor)
-      else:
-        if arrived_time[index_neighbor] > arrived_time[index_tmp] + 1:
+
+  try:
+    index_item
+    arrived_time[index_google] = 0
+    queue = [index_google]
+    
+    while queue:
+      index_tmp = queue.pop(0)
+      for index_neighbor in links[index_tmp]:
+        if arrived_time[index_neighbor] < 0:
+          arrived_time[index_neighbor] = arrived_time[index_tmp] + 1
+          prev_spot[index_neighbor] = index_tmp
+          if index_neighbor in links:
+            queue.append(index_neighbor)
+        elif arrived_time[index_neighbor] > arrived_time[index_tmp] + 1:
           arrived_time[index_neighbor] = arrived_time[index_tmp] + 1
           prev_spot[index_neighbor] = index_tmp
 
-  return arrived_time, prev_spot, index_item
+    return arrived_time, prev_spot, index_item
+  except NameError:
+    print("The graph doesn't have the word")
+    exit(1)
 
 def main():
   pages = {}
   links = {}
+  searched_word = sys.argv[1]
 
   max_page_id = 0
-  with open('data/pages.txt') as f:
+  with open('data/pages_small.txt') as f:
     for data in f.read().splitlines():
       page = data.split('\t')
       # page[0]: id, page[1]: title
       pages[int(page[0])] = page[1]
       max_page_id = max(max_page_id, int(page[0]))
 
-  with open('data/links.txt') as f:
+  with open('data/links_small.txt') as f:
     for data in f.read().splitlines():
       link = data.split('\t')
       # link[0]: id (from), links[1]: id (to)
@@ -71,7 +56,7 @@ def main():
   prev_spot = [""] * (max_page_id + 1)
   
   route = []
-  arrived_time, prev_spot, tmp = bfs(pages, links, arrived_time, prev_spot, "渋谷")
+  arrived_time, prev_spot, tmp = bfs(pages, links, arrived_time, prev_spot, searched_word)
   if arrived_time[tmp] == -1:
     print("is not connected")
   else:
